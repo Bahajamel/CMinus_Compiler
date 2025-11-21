71c38dc8f2aa96e68d95e3a8f8056676bd3b5bf0
yesminedjebbi <yesminedyesmine@gmail.com>
2025-11-21 15:08:54 +0100

    test

--- test.c
+++ test.c
@@ -1,5 +1,36 @@
-int f(int x) {
-  int a, b;
-  a = 5;
-  return x;
+int x;
+int y;
+int i; 
+int add(int a, int b) { return a + b; }
+int main() {
+    x = 1;
+    y = 3;
+
+    x = add(x, y);
+     {
+        int z;
+        z = x + y;
+        y = z;
+    }
+     if (x > y) {
+        x = x - 1;
+    } else {
+        y = y - 1;
+    }
+    while (x < 10) {
+        x = x + 1;
+    }
+     do {
+        y = y - 1;
+    } while (y > 0);
+
+   for (i = 0; i < 5; i = i + 1) {
+        x = x + i;
+    }
+    int * p;
+    
+
+    
+    return 0;
 }
+
